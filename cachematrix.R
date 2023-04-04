4aa6ec0444fce6a572163096773f93c1b0952e61
npwi223 <69438684+npwi223@users.noreply.github.com>
2023-04-03 20:28:35 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Demonstrate how to solve a matrix
+hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, `+`) }
+h8 <- hilbert(8)
+sh8 <- solve(h8)
 
-## Write a short comment describing this function
+## These functions solve and return matrix inverses
+
+## This is a function that establishes a cached matrix with its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+
+## This is a function that returns the cached solve/inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached solve")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s      ## Return a matrix that is the inverse of 'x'
+
+  
 }
+
+# Demonstrate the function working
+h8cache <- makeCacheMatrix(h8)
+cacheSolve(h8cache)
